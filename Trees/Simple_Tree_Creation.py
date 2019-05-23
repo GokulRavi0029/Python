b63918edc9d6e2da0123a2d0c8ba96a31a6da3d1
GokulRavi0029 <50522975+GokulRavi0029@users.noreply.github.com>
2019-05-23 20:12:34 +0530

    Update Simple_Tree_Creation.py

--- Trees/Simple_Tree_Creation.py
+++ Trees/Simple_Tree_Creation.py
@@ -1,9 +1,11 @@
+#creation of node
 class node():
     def __init__(self,data):
         self.left=None
         self.right=None
         self.data=data
     
+    #function for insertion of data into tree
     def insertdata(self,data):
         if self.data:
             if data < self.data:
@@ -18,7 +20,8 @@ class node():
                     self.right.insertdata(data)
         else:
             self.data=data
-
+    
+    #function to print tree
     def printnode(self):
         res = []
         if self.left:
@@ -27,7 +30,7 @@ class node():
         if self.right:
             self.right.printnode()
 
-
+# used the insert method to add nodes
 root = node(12)
 root.insertdata(6)
 root.insertdata(14)
