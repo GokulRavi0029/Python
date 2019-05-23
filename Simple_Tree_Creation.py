4ceaf285d7bafcac76aa433bf78e6fccefd42a54
GokulRavi0029 <50522975+GokulRavi0029@users.noreply.github.com>
2019-05-23 20:06:36 +0530

    Add files via upload

---
+++ Simple_Tree_Creation.py
@@ -0,0 +1,36 @@
+class node():
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.data=data
+    
+    def insertdata(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left=node(data)
+                else:
+                    self.left.insertdata(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right=node(data)
+                else:
+                    self.right.insertdata(data)
+        else:
+            self.data=data
+
+    def printnode(self):
+        res = []
+        if self.left:
+            self.left.printnode()
+        print(self.data),
+        if self.right:
+            self.right.printnode()
+
+
+root = node(12)
+root.insertdata(6)
+root.insertdata(14)
+root.insertdata(3)
+root.insertdata(4)
+root.printnode()
